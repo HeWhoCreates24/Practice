573727bfed3d88b15681a3dca21745efc7659922
Tanmay Hatkar <tanmayhatkar240@gmail.com>
2024-12-14 16:41:10 +0530

    classes in js

--- tanmayJs/jsBasics/script.js
+++ tanmayJs/jsBasics/script.js
@@ -1,31 +1,32 @@
-let p = document.querySelector("p");
-p.classList.add("changeColor");
-
-const body = document.querySelector("div");
-for (let i = 0; i < 3; i++){
-    body.innerHTML += `<div class="box">box ${i+1}</div>\n`
+class user {
+    constructor(name, mail){
+        this.name = name;
+        this.mail = mail;
+    }
+    viewData(str) {
+        if (str === "name"){
+            return this.name;
+        }
+        else if (str === "mail"){
+            return this.mail;
+        }
+        else{
+            return "WTF do you want?"
+        }
+    }
 }
-
-let newButton = document.createElement("button");
-newButton.innerHTML = `<b>CLICK ME!</b>`;
-newButton.style.backgroundColor = "red";
-newButton.style.color = "white";
-document.querySelector("body div").prepend(newButton);
-let btn = document.querySelector("button");
-btn.addEventListener("click", () => {
-    let rootStyle = getComputedStyle(document.documentElement);
-    if (rootStyle.getPropertyValue("--theme").trim() === "light"){
-        document.documentElement.style.setProperty("--c1", "darkblue");
-        document.documentElement.style.setProperty("--c2", "darkgreen");
-        document.documentElement.style.setProperty("--c3", "white");
-        document.documentElement.style.setProperty("--c4", "black");
-        document.documentElement.style.setProperty("--theme", "dark");
+class admin extends user {
+    editData(str) {
+        if (str === "name"){
+            this.name = prompt("enter new name");
+            return this.name;
+        }
+        else if (str === "mail"){
+            this.mail = prompt("enter new mail");
+            return this.mail;
+        }
+        else{
+            return "WTF do you want?"
+        }
     }
-    else{
-        document.documentElement.style.setProperty("--c1", "pink");
-        document.documentElement.style.setProperty("--c2", "lightblue");
-        document.documentElement.style.setProperty("--c3", "black");
-        document.documentElement.style.setProperty("--c4", "white");
-        document.documentElement.style.setProperty("--theme", "light");
-    }
-})
+}

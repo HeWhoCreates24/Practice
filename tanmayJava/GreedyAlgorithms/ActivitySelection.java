9614d76a76c4ac0c0145e2fa330d55a574885216
Tanmay Hatkar <tanmayhatkar240@gmail.com>
2025-01-10 16:52:08 +0530

    Using comparator for 2d arr sorting!!

--- tanmayJava/GreedyAlgorithms/ActivitySelection.java
+++ tanmayJava/GreedyAlgorithms/ActivitySelection.java
@@ -1,5 +1,7 @@
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 
 public class ActivitySelection{
 
@@ -8,23 +10,36 @@ public class ActivitySelection{
         int[] st = {1, 3, 0, 5, 8, 5};
         int[] en = {2, 4, 6, 7, 9, 9};
 
+        int[][] arr = new int[st.length][3];
+
+        for (int i = 0; i < st.length; i++){
+
+            arr[i][0] = i;
+            arr[i][1] = st[i];
+            arr[i][2] = en[i];
+
+        }
+
+        Arrays.sort(arr, Comparator.comparingDouble(o -> o[2]));
+
         int acts = 0;
         ArrayList <Integer> ans = new ArrayList<>();
         int enTime = 0;
 
         for (int i = 0; i < st.length; i++){
 
-            if (st[i] >= enTime){
+            if (arr[i][1] >= enTime){
                 acts++;
-                ans.add(i);
-                enTime = en[i];
+                ans.add(arr[i][0]);
+                enTime = arr[i][2];
             }
 
         }
 
         for (int i : ans){
-            System.out.println("A" + i + " ");
+            System.out.print("A" + i + " ");
         }
+        System.out.println();
         System.out.println("Activities done : " + acts + " out of " + st.length);
 
     }

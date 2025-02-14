89360a6690bf777af35c20abe8bea4b9c449eedd
Tanmay Hatkar <tanmayhatkar240@gmail.com>
2025-02-14 10:31:27 +0530

    cycle detection

--- tanmayJava/Graphs/CycleDetection.java
+++ tanmayJava/Graphs/CycleDetection.java
@@ -1,47 +1,52 @@
-public class CycleDetection{
-    public static boolean hasCycle(GraphImplementation.Graph g){
-        boolean[] visited = new boolean[g.size];
+public class CycleDetection {
+    public static boolean hasCycle(GraphImplementation.Graph g) {
+        boolean[] visited = new boolean[g.size]; // Keeps track of visited nodes
 
-        for (int i = 0; i < g.size; i++){
-            if (hasCycleUtil(g, visited, i, -1)){
+        // Check for cycles in each connected component
+        for (int i = 0; i < g.size; i++) {
+            if (!visited[i]) {  // Only start DFS if node is not visited
+                if (hasCycleUtil(g, visited, i, -1)) {
+                    return true; // Cycle detected
+                }
+            }
+        }
+        return false; // No cycle found
+    }
+
+    private static boolean hasCycleUtil(GraphImplementation.Graph g, boolean[] visited, int curr, int parent) {
+        visited[curr] = true;  // Mark the current node as visited
+
+        for (GraphImplementation.Edge e : g.getEdges(curr)) {  
+            int neighbor = e.end;
+
+            if (!visited[neighbor]) {  
+                // Recur for unvisited nodes
+                if (hasCycleUtil(g, visited, neighbor, curr)) {
+                    return true;
+                }
+            } else if (neighbor != parent) {  
+                // If visited node is not the parent, a cycle is found
                 return true;
             }
         }
         return false;
     }
 
-    public static boolean hasCycleUtil(GraphImplementation.Graph g, boolean[] visited, int curr, int parent){
-        visited[curr] = true;
-
-        for (GraphImplementation.Edge e : g.getEdges(curr)){
-            if (visited[e.end] && e.end != parent){
-                return true;
-            }
-        }
-        return false;
-    }
     public static void main(String[] args) {
         GraphImplementation.Graph g = new GraphImplementation.Graph(5);
 
         g.addEdge(0, 1, 1);
-        // g.addEdge(0, 2, 1);
         g.addEdge(0, 3, 1);
-
-        g.addEdge(1, 0, 1);
         g.addEdge(1, 2, 1);
-        
-        // g.addEdge(2, 0, 1);
-        g.addEdge(2, 1, 1);
-        
-        g.addEdge(3, 0, 1);
         g.addEdge(3, 4, 1);
-        
-        g.addEdge(4, 3, 1);
 
-        if (hasCycle(g)){
-            System.out.println("cycle detected");
-        }else{
-            System.out.println("cycle NOT detected");
+        // Uncommenting the line below will introduce a cycle
+        // g.addEdge(2, 0, 1);
+
+        if (hasCycle(g)) {
+            System.out.println("Cycle detected");
+        } else {
+            System.out.println("Cycle NOT detected");
         }
     }
 }
